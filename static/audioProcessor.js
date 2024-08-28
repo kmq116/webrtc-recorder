82f1849f76cb68c43351a5ae673e1415f188533b
飞天御剑流co <521luqiya@gmail.com>
2024-08-28 11:40:06 +0800

    feat: 添加可以设置采样率的配置

--- static/audioProcessor.js
+++ static/audioProcessor.js
@@ -1,19 +1,37 @@
 class AudioProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.targetSampleRate = 8000;
+  }
   process(inputs, outputs, parameters) {
     const input = inputs[0];
     const inputData = input[0];
 
-    // 重采样到 8000Hz
-    const ratio = sampleRate / 8000;
-    const newLength = Math.round(inputData.length / ratio);
-    const result = new Int16Array(newLength);
-
-    for (let i = 0; i < newLength; i++) {
-      // 将 Float32 转换为 Int16
-      result[i] = Math.max(-32768, Math.min(32767, Math.round(inputData[Math.round(i * ratio)] * 32767)));
+    if (!inputData) {
+      return true;
     }
 
-    this.port.postMessage(result);
+
+    if (sampleRate !== this.targetSampleRate) {
+      const ratio = sampleRate / this.targetSampleRate;
+      const newLength = Math.round(inputData.length / ratio);
+      const result = new Int16Array(newLength);
+
+      for (let i = 0; i < newLength; i++) {
+        const index = Math.min(Math.round(i * ratio), inputData.length - 1);
+        result[i] = Math.max(-32768, Math.min(32767, Math.round(inputData[index] * 32767)));
+      }
+
+      this.port.postMessage(result);
+    } else {
+      // If sample rate is already 48000Hz, just convert to Int16Array
+      const result = new Int16Array(inputData.length);
+      for (let i = 0; i < inputData.length; i++) {
+        result[i] = Math.max(-32768, Math.min(32767, Math.round(inputData[i] * 32767)));
+      }
+      this.port.postMessage(result);
+    }
+
     return true;
   }
 }
